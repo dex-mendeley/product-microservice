cfed3fac4b53e316f70b9c12279a34d511afc1fb
Dex <k.decker@elsevier.com>
2021-02-12 16:23:43 +0000

    Added get, delete and patch endpoints

--- src/main/java/com/mendeley/test/product_microservice/controller/BookController.java
+++ src/main/java/com/mendeley/test/product_microservice/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -35,4 +36,72 @@ public class BookController {
         return entity;
     }
 
+    @GetMapping()
+    public ResponseEntity<Book> getAllBooks() {
+        ResponseEntity entity;
+
+        try {
+            List<Book> books = bookRepository.findAll();
+            entity = new ResponseEntity(books, HttpStatus.OK);
+        } catch (Exception e) {
+            entity = new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return entity;
+    }
+
+    @DeleteMapping("/{isbn}")
+    public ResponseEntity<Book> deleteBook(@PathVariable String isbn) {
+        ResponseEntity entity;
+
+        try {
+            Optional<Book> opt = bookRepository.findById(isbn);
+
+            if (opt.isPresent()) {
+                bookRepository.deleteById(isbn);
+                entity = new ResponseEntity(HttpStatus.OK);
+            } else {
+                entity = new ResponseEntity(HttpStatus.NOT_FOUND);
+            };
+            return entity;
+
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping()
+    public ResponseEntity<Book> updatedBook(@RequestBody Book book) {
+        ResponseEntity entity;
+
+        Optional<Book> opt = bookRepository.findById(book.getIsbn());
+        if(opt.isPresent()) {
+            Book updatedBook = new Book();
+            Book savedBook = opt.get();
+
+            updatedBook.setIsbn(book.getIsbn());
+
+            if(book.getTitle() != null) {
+                updatedBook.setTitle(book.getTitle());
+            } else {
+                updatedBook.setTitle(savedBook.getTitle());
+            }
+
+            if(book.getAuthor_first() != null) {
+                updatedBook.setAuthor_first(book.getAuthor_first());
+            } else {
+                updatedBook.setAuthor_first(savedBook.getAuthor_first());
+            }
+
+            if(book.getAuthor_last() != null) {
+                updatedBook.setAuthor_last(book.getAuthor_last());
+            } else {
+                updatedBook.setAuthor_last(savedBook.getAuthor_last());
+            }
+
+            return new ResponseEntity(updatedBook, HttpStatus.OK);
+        } else {
+            return null;
+        }
+    }
 }
--- src/test/java/com/mendeley/test/product_microservice/controller/BookControllerTest.java
+++ src/test/java/com/mendeley/test/product_microservice/controller/BookControllerTest.java
@@ -11,8 +11,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,6 +35,7 @@ public class BookControllerTest {
     ObjectMapper objectMapper;
 
     Book testBook = new Book("mock_isbn", "test_title", "test_firstName", "test_lastName");
+    Book testBook2 = new Book("mock_isbn_2", "Second title", "Brown", "Dan");
     Book noISBN = new Book("test_title", "test_firstName", "test_lastName");
 
     @Test
@@ -61,5 +66,65 @@ public class BookControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void getAllBooks() throws Exception {
+        when(repository.findAll()).thenReturn(List.of(testBook, testBook2));
+
+        mockMvc.perform(get("/books"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(List.of(testBook, testBook2))));
+    }
+
+    @Test
+    public void testServerError() throws Exception {
+        when(repository.findAll()).thenThrow(new RuntimeException("Something went wrong"));
+
+        mockMvc.perform(get("/books"))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    public void deleteBook() throws Exception {
+        when(repository.findById("foobar")).thenReturn(Optional.ofNullable(testBook));
+
+        mockMvc.perform(delete("/books/foobar"))
+                .andExpect(status().isOk());
+    }
+
+    // 404
+    @Test
+    public void deleteUnknownBook() throws Exception {
+        when(repository.findById("unknown")).thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/books/unknown"))
+                .andExpect(status().isNotFound());
+    }
+
+    // 500
+    @Test
+    public void serverError() throws Exception {
+        when(repository.findById(anyString())).thenThrow(new RuntimeException("Something went wrong"));
+
+        mockMvc.perform(delete("/books/unknown"))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    public void updateBook() throws Exception {
+        Book updatedBook = new Book("mock_isbn", "updated_title", "test_firstName", "test_lastName");
+        Book update = new Book("mock_isbn", "updated_title", null, null);
+
+        when(repository.findById("mock_isbn")).thenReturn(Optional.ofNullable(updatedBook));
+
+
+        mockMvc.perform(patch("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(update)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(updatedBook)));
+
+    }
+
 
 }
